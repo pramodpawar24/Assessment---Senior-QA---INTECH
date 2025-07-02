6d6938e0599613bc3bb449160919680bcbcd51fa
pramodpawar24 <pramod.pawar@researchwire.in>
2025-07-02 12:27:33 +0530

    updating files

--- src/test/java/java/base/BaseTest.java
+++ src/test/java/java/base/BaseTest.java
@@ -2,13 +2,17 @@ package java.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 import java.time.Duration;
 
 public class BaseTest {
     protected WebDriver driver;
 
     public void initializeDriver() {
-        System.setProperty("webdriver.chrome.driver", "/FRAND_Avanue_Project_Staging_Test/Configuration/chromedriver.exe");
+       // System.setProperty("webdriver.chrome.driver", "/FRAND_Avanue_Project_Staging_Test/Configuration/chromedriver.exe");
+    	WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
